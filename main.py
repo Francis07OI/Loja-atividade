7eb8e48359983bbcc3025e9ed7379a01efd24e7a
francisco <franciscoaraujo1076@gmail.com>
2026-05-04 11:19:38 -0300

    Criação do Home

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from fastapi import FastAPI, Request, Depends, Form, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+ 
+from database import get_db
+from models import Categoria, Produto
+ 
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+ 
+
+# HOME
+
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request, db: Session = Depends(get_db)):
     """Página inicial com totais de categorias e produtos."""
@@ -6,3 +20,7 @@ def index(request: Request, db: Session = Depends(get_db)):
         "total_categorias": db.query(Categoria).count(),
         "total_produtos": db.query(Produto).count(),
     })
+ 
+ 
+
+cria
